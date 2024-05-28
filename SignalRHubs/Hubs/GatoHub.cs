8404aec8a7b7c5c8f17e8229e9f494789c3b3f13
AngelPineda1 <201G0276@rcarbonifera.tecnm.mx>
2024-05-28 12:44:48 -0600

    update

--- SignalRHubs/Hubs/GatoHub.cs
+++ SignalRHubs/Hubs/GatoHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRHubs.Models;
 
 namespace SignalRHubs.Hubs
 {
@@ -6,6 +7,11 @@ namespace SignalRHubs.Hubs
     {
 
         public static Dictionary<string, string> usuarios = new Dictionary<string, string>();// existe para todas las instancias
+        
+        
+        public static Dictionary<string,Partida> Partidas=new Dictionary<string,Partida>();
+        
+        
         public async Task IniciarSesion(string nombreUsuario)
         {
             //verifica si el usuario esta en uso
@@ -45,6 +51,17 @@ namespace SignalRHubs.Hubs
                 NumPartida++;
                 await Clients.Groups(partida).SendAsync("Game Started",partida);
                 await Clients.Users(Context.ConnectionId).SendAsync("Play");
+
+                var datosPartida = new Partida()
+                {
+                    NombrePartida = partida,
+                    NombreUsuario1 = nombreUsuario,
+                    NombreUsuario2 = contrincante,
+                    ConnectionId1 = Context.ConnectionId,
+                    ConnectionId2 = usuarios[contrincante],
+                    Turno = 'X'
+                };
+                Partidas[partida] = datosPartida;
             }
         }
 
@@ -52,10 +69,31 @@ namespace SignalRHubs.Hubs
 
         public async Task Jugar(string partida, string nombreUsuario,string tablero)
         {
+            var datosPartida = Partidas[partida];
+            if(GanoXO(datosPartida.Turno,tablero))
+            {
 
+            }
+            else
+            {
+                datosPartida.Turno = datosPartida.Turno == 'X' ? '0' : 'X';
+            }
         }
 
-        
+        private bool GanoXO(char turno, string tablero)
+        {
+            
+        }
+
+        int[,] lineas = new int[,]
+        {
+            {0,1,2 },   //...........
+            {3,4,5 },
+            {6,7,8 },
+            {0,3,6 },
+            {1,4,7 },
+            {2,5,8 }
+        };
 
 
     }
---
+++ SignalRHubs/Models/Partida.cs
@@ -0,0 +1,15 @@
+﻿namespace SignalRHubs.Models
+{
+    public class Partida
+    {
+        public string NombrePartida { get; set; }
+        public string NombreUsuario1 { get; set; }
+        public string NombreUsuario2 { get; set; }
+        public string ConnectionId1 { get; set; }
+        public string ConnectionId2 { get; set; }
+        public char Turno { get; set; }
+
+
+
+    }
+}
